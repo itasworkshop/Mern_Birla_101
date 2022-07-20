bb60b35d6986dbc47ad6cce0a9263b071806e9a5
Nadeem khan <nadeemkhanpathan80@gmail.com>
2022-07-20 15:20:34 +0530

    .

--- students/Nadim/excercise/Q3.js
+++
@@ -1,3 +0,0 @@
-// WAP to validate password and email in signup form(HTML,CSS and Javascript)
-// 	a. password ca not be less than 6 character and at least one capital letter.
-// 	b. email should include valid domain (eg: @gmail.com).
