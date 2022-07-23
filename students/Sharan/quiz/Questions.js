a1e42954b4b7baf7e9510f62ea40293b4c5b4e42
SharanGadheri <sharanthegadheri@gmail.com>
2022-07-24 00:43:06 +0530

    Updated File

---
+++ students/Sharan/quiz/Questions.js
@@ -0,0 +1,14 @@
+WAP to check whether given matrix is identical matrix or not for 2*2 and 3*3 matrix?
+
+WAP to make reverse of the string using loops.
+
+WAP to sort array of your choice using loops.
+
+
+
+
+WAP to check whether given number or string is palindrom or not?
+
+
+
+
