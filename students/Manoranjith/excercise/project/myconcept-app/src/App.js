a88f58dd8cc711c912b033784eab92cd833b29bd
26mano <[mmanoranjith1997@gmail.com]>
2022-07-25 19:35:41 +0530

    25 july React-code

--- students/Manoranjith/excercise/project/myconcept-app/src/App.js
+++ students/Manoranjith/excercise/project/myconcept-app/src/App.js
@@ -1,11 +1,24 @@
 
+import { useState } from 'react';
 import Numbers from './Components/Numbers';
 
-function App(props) {
+function App() {
+  const [num, setNum] = useState({
+    number1: '',
+    number2:''
+  });
 
   return (
     <div >
-      <Numbers />
+      <h1>
+        Calculator
+      </h1>
+      <div>
+      <input type="text" value={num.number1} onChange={(e) => {setNum({...num, number1:e.target.value})}} />
+      <input type="text" value={num.number2} onChange={(e) => {setNum({...num, number2:e.target.value})}} />
+      </div><br />
+
+      <Numbers number1={num.number1} number2= {num.number2}  />
     </div>
   );
 }
--- students/Manoranjith/excercise/project/myconcept-app/src/Components/Numbers.js
+++ students/Manoranjith/excercise/project/myconcept-app/src/Components/Numbers.js
@@ -2,63 +2,67 @@ import React,{useState} from 'react'
 
 export default function Numbers(props) {
 
-    const [state, setState] = useState(
-      {
-        number1: '',
-        number2: '',
-        tot: ''
-      });
+    const [total, setTotal] = useState({
+      addition :'',
+      subtraction: '',
+      multiplication: '',
+      division:''
+    });
+
+
     
       const addition = () => {
-        setState({
-          ...state,
-           tot: parseInt(state.number1) + parseInt(state.number2) });
-        console.log(state);
+        setTotal( { 
+          ...total,
+          addition: parseInt(props.number1) + parseInt(props.number2)} );
+        console.log(total);
       }
       function subtraction(){
-        setState({
-          ...state,
-          tot: parseInt(state.number1) - parseInt(state.number2)
-        });
+        setTotal(
+          { 
+            ...total,
+            subtraction : parseInt(props.number1) - parseInt(props.number2)}
+        );
       }
       function multiplication(){
-        setState({
-          ...state,
-          tot: parseInt(state.number1) * parseInt(state.number2)
-        });
+        setTotal(
+           {
+            ...total,
+           multiplication: parseInt(props.number1) * parseInt(props.number2)}
+        );
       }
       function division(){
-        setState({
-          ...state,
-          tot: parseInt(state.number1) / parseInt(state.number2)
-        });
+        setTotal(
+          {...total,
+          division: parseInt(props.number1) / parseInt(props.number2)}
+        );
       }
     
       return (
         <div >
-            <div>
-              <h3> Calculator </h3>
-            </div>
-             <>
-             <input type="text" value={state.number1} 
-             onChange={(e) => {setState({ ...state , number1 : e.target.value})} } />
-             <input type="text" value={state.number2} 
-             onChange={(e) => {setState({ ...state , number2 : e.target.value})}} />
-             </>
+            
             <div>
               <button onClick={addition} >Add</button> 
+              
+              <input type="text" value={total.addition} />
             </div>
             <div>
               <button onClick={subtraction} >Sub</button> 
+              
+              <input type="text" value={total.subtraction} />
             </div>
             <div>
               <button onClick={multiplication} >Multiple</button> 
+              
+              <input type="text" value={total.multiplication} />
             </div>
             <div>
               <button onClick={division} > Divide </button> 
+              
+              <input type="text" value={total.division} />
             </div>
             <div>
-            <input type="text" value={state.tot} />
+            
             </div>
             
         </div>
