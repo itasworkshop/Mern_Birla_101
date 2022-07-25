b9b3fdd5c227cb826e718d64062737662e7672b4
Pravin0545 <pravinch007@gmail.com>
2022-07-25 14:34:55 +0530

    25 july my_project

---
+++ students/Pravin/project/my_project/src/Component/Calculator.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+export function Calculator() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <input type="text" onChange={(e) => setNum1(e.target.value)} />
+      <input type="text" onChange={(e) => setNum2(e.target.value)} />
+      <h1>
+        Addition:-{Number(num1) + Number(num2)}
+        <br />
+        Substraction:-{Number(num1) - Number(num2)}
+        <br />
+        Division:-{Number(num1) / Number(num2)}
+        <br />
+        Multiplication:-{Number(num1) * Number(num2)}
+      </h1>
+    </div>
+  );
+}
