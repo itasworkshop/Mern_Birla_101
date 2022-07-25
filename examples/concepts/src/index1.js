566f7658d149725093b937e562bb099f6296298d
itasworkshop <itasworkshop@gmail.com>
2022-07-25 11:56:42 +0530

    25 July 1

---
+++ examples/concepts/src/index1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+
+function App(){
+  return <h1>Hello World!!!</h1>;
+}
+
+ReactDOM.render(
+  <App/>, document.getElementById('root')
+);
+
