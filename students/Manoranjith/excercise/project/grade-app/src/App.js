3f4bc9409f43fc14be252553965b097dfdbc1e95
26mano <[mmanoranjith1997@gmail.com]>
2022-07-25 21:35:40 +0530

    25 july React code

--- students/Manoranjith/excercise/project/grade-app/src/App.js
+++ students/Manoranjith/excercise/project/grade-app/src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import GradeCalculation from "./Grade/GradeCalculation";
 
 function App() {
+  const [users, setusers] = useState([])
+  const [state, setState] = useState({
+    name:'',
+    number1: '',
+    number2:'',
+    number3:'',
+    Grade:''
+  });
+
+  
+  const calculateGrade = () => {
+   let avg = (parseInt(state.number1) + parseInt(state.number2) + parseInt(state.number3))/3;
+    if(avg >=90 && avg <= 100){
+      setState({...state, Grade : "A"});
+    }else if(avg>=80 && avg<90){
+      setState({...state, Grade : "B"});
+    }else if(avg>=70 && avg<80){
+      setState({...state, Grade : "C"});
+    }else {
+      setState({...state, Grade : "U"});
+    }
+  }
+
+  const addStudent = () => {
+    setusers([...users, state]);
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <tr>
+        <input type="text" placeholder='Name' value={state.name} onChange={(e) => {setState({...state,name: e.target.value})}}  />
+        <input type="text" placeholder='Chemistry Mark' value={state.number1} onChange={(e) => {setState({...state, number1: e.target.value})}}  />
+        <input type="text" placeholder='Physics Mark' value={state.number2} onChange={(e) => {setState({...state, number2: e.target.value})}}  />
+        <input type="text" placeholder='Maths Mark' value={state.number3} onChange={(e) => {setState({...state, number3: e.target.value})}}  />
+        </tr>
+        <div style={{ m:"20px"}}>
+        <button onClick={calculateGrade} style={{ p:"20px", width:"300px", backgroundColor:"Yellow"}} >Grade</button>
+        <button onClick={addStudent} style={{ p:"20px", width:"300px", backgroundColor:"Yellow"}} >Add Student</button>
+        </div>
+
+     <GradeCalculation  name={state.name} cmark={state.number1} pmark={state.number2} mmark={state.number3} grade={state.Grade} student={users} />
       </header>
     </div>
   );
---
+++ students/Manoranjith/excercise/project/grade-app/src/Grade/GradeCalculation.js
@@ -0,0 +1,99 @@
+import React from 'react';
+
+
+// const students = [
+//     {id:101,name:"raj",cmarks:45,pmarks:55,mmarks:67},
+//       {id:102,name:"rajesh",cmarks:65,pmarks:85,mmarks:77},
+//       {id:103,name:"suraj",cmarks:43,pmarks:55,mmarks:60},
+//       {id:104,name:"tom",cmarks:71,pmarks:65,mmarks:70}
+//       ];
+
+export default function App(props){
+  return ( 
+    <div >
+      <header >
+        <div>
+          <h3> Student Grade Calculator </h3>
+        </div>
+        <div>
+            {props.name !== '' &&
+          <table cellSpacing={10}>
+            <thead>
+              <tr>
+              <td>Student Name</td>
+              <td>Chem mark</td>
+              <td>Physic mark</td>
+              <td>Maths mark</td>
+              <td>Grade</td>
+              </tr>
+              
+              </thead>
+            <tbody>
+              
+                  <tr>
+                   <td >
+                    {props.name}
+                   </td>
+                   <td >
+                    {props.cmark}
+                   </td>
+                   <td >
+                    {props.pmark}
+                   </td>
+                   <td >
+                    {props.mmark}
+                   </td>
+                   <td >
+                    {props.grade}
+                   </td>
+                   </tr>
+            </tbody>
+          </table>
+}
+        </div>
+        <div>
+            {props.student.length > 0 &&
+        <table cellSpacing={10}>
+        <thead>
+              <tr>
+              <td>Student Name</td>
+              <td>Chem mark</td>
+              <td>Physic mark</td>
+              <td>Maths mark</td>
+              <td>Grade</td>
+              </tr>
+              
+              </thead>
+           {props.student.map((stu) => (
+           
+            
+            <tbody>
+              
+                  <tr>
+                   <td >
+                    {stu.name}
+                   </td>
+                   <td >
+                    {stu.number1}
+                   </td>
+                   <td >
+                    {stu.number2}
+                   </td>
+                   <td >
+                    {stu.number3}
+                   </td>
+                   <td >
+                    {stu.Grade}
+                   </td>
+                   </tr>
+            </tbody>
+         
+           ))}
+            </table>
+}
+        </div>
+      </header>
+    </div>
+  );
+}
+
