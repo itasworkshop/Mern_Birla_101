78acffa386acb6fa744fe8072800d909de2f7887
itasworkshop <itasworkshop@gmail.com>
2022-07-26 10:22:47 +0530

    26 July 1

--- examples/concepts/src/index.js
+++ examples/concepts/src/index.js
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-<<<<<<< HEAD
 
 
 function MyHello(){
@@ -17,20 +16,9 @@ function YourHello(){
 function LoginButton(props){
   return(
     <button onClick={props.onClick}>Login</button>
-=======
-//React DOM apply changes only where it requires.
-
-function tick() {
-  const elements = (
-    <div>
-      <h1>This is demo for Clock Tick.</h1>
-      <h2>This is {new Date().toString()}</h2>
-    </div>
->>>>>>> 288ee7a50740b6cb5ec968da85f1ee6bd9f13328
   );
 }
 
-<<<<<<< HEAD
 function LogoutButton(props){
   return(
     <button onClick={props.onClick}>Logout</button>
@@ -87,11 +75,3 @@ class LoginComponent extends React.Component{
 ReactDOM.render(
   <LoginComponent />, document.getElementById('root')
 );
-=======
-  ReactDOM.render(elements, document.getElementById("root"));
-}
-
-setInterval(() => {
-  tick();
-}, 1000);
->>>>>>> 288ee7a50740b6cb5ec968da85f1ee6bd9f13328
