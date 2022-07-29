d87511f21bdab6a0cf8983ec352cf2d5034b79ed
anamsiddiqui-07 <asra.anam94@gmail.com>
2022-07-29 14:55:15 +0530

    29th_july

--- students/Asra/project/react_project1/src/App.js
+++ students/Asra/project/react_project1/src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
+import { Studentgradehooks } from './29thJuly/Studentgradehooks';
 import './App.css';
-import { Currency } from './Components/26thjuly/Currency';
-import { Login } from './Components/26thjuly/Login';
+// import { Currency } from './Components/26thjuly/Currency';
+// import { Login } from './Components/26thjuly/Login';
 // import { Toggleimg } from './Components/26thjuly/Toggleimg';
 // import { Calculator } from './Components/Calculator';
 // import { Studentgrade } from './Components/Studentgrade';
@@ -20,7 +21,9 @@ function App() {
       {/* image1="https://downloadhdwallpapers.in/wp-content/uploads/2018/09/Round-Sphere-Moving-Animated-Download-Gif-Image.gif"/> */}
 
       {/* <Login pass= "12345"/> */}
-      <Currency dollar="20" Rupees="79"/>
+      {/* /* <Currency dollar="20" Rupees="79"/> */}
+      <Studentgradehooks/>
+      
       
     </div>
   );
