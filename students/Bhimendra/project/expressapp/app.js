7661798b9775d2b61efe86dda083d28f122dc5ae
Bhimendra <106160891+Bhimendra@users.noreply.github.com>
2022-08-09 11:07:27 +0530

    express

---
+++ students/Bhimendra/project/expressapp/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 4444
+
+
+app.get("/calc/:num1/:num2", (req, res) => {
+  const addition = Number(req.params.num1) + Number(req.params.num2);
+  const ssubstraction = Number(req.params.num1) - Number(req.params.num2);
+  const multiply = Number(req.params.num1) * Number(req.params.num2);
+  const division = Number(req.params.num1) / Number(req.params.num2);
+  res.send(
+    `"Hello this is addition page response!"  ${addition}  "Hello this is substraction page response!"  ${ssubstraction}  "Hello this is multiplication page response!"  ${multiply}  "Hello this is division page response!"  ${division}`
+  );
+});
+
+app.get("/grade/:num", (req, res) => {
+  if (req.params.num > 90 && req.params.num <= 100) {
+    res.send("Grade A");
+  } else if (req.params.num > 80 && req.params.num <= 90) {
+    res.send("Grade B");
+  } else if (req.params.num > 70 && req.params.num <= 80) {
+    res.send("Grade c");
+  } else if (req.params.num <= 70) {
+    res.send("Fail");
+  }
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
