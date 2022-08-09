d7010f118c5ec695fa2dd2db08747557f62b0a1c
26mano <[mmanoranjith1997@gmail.com]>
2022-08-09 12:55:06 +0530

    09 Aug

---
+++ students/Manoranjith/excercise/project/students-express/src/Router/router.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = new express.Router();
+const crypto = require("crypto");
+
+router.get('/students/:name/:cmark/:mmark/:pmark', (req, res) => {
+    const name = req.params.name;
+    const cmark = req.params.cmark;
+    const mmark = req.params.mmark;
+    const pmark = req.params.pmark;
+    const id = crypto.randomBytes(3*4).toString("hex");
+    const total = parseInt(cmark ) + parseInt(mmark) + parseInt(pmark)
+      let avg = (total)/3;
+      let Grade =""
+       if(avg >=90 && avg <= 100){
+         Grade = "A";
+       }else if(avg>=80 && avg<90){
+          Grade = "B";
+       }else if(avg>=70 && avg<80){
+         Grade = "C";
+       }else {
+         Grade = "U";
+       }
+
+       res.json({"id" : id , "Name" : name ,"Chemistry" : cmark, "Maths" : mmark,"Physics" : pmark,"Total" : total, "Average" : avg, "Grade" : Grade   });
+       const data = localStorage.setItem('date', {"name":name, "cmark":cmark, "mmark": mmark , "pmark" : pmark , "total": total})
+
+}) 
+
+module.exports = router
--- students/Manoranjith/excercise/project/students-express/src/app.js
+++ students/Manoranjith/excercise/project/students-express/src/app.js
@@ -1,41 +1,24 @@
 const express = require('express')
+const router = require("./Router/router")
 const app = express()
 const port = 3000
 
 
-//get request for home path
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-//http://localhost:3000/students
-// app.get('/students', (req, res) => {
-//     res.send('Hello from students page response!')
-// })
-
-//http://localhost:3000/students/rajesh
-app.get('/students/:id', (req, res) => {
-    const id = req.params.id;
-    res.send('Hello from students page response!'+id)
+app.get('/calculator/:number1/:number2', (req, res) => {
+  const number1 = req.params.number1;
+  const number2 = req.params.number2;
+  let add = parseFloat(number1) + parseFloat(number2);
+  let multi = parseFloat(number1) * parseFloat(number2);
+  let divide = parseFloat(number1) / parseFloat(number2);
+  let subtraction = parseFloat(number1) - parseFloat(number2);
+  res.json({"Number1" : number1, "Number2" : number2 ,"Addition" : add ,"Subtration" :subtraction, "Multiply" : multi, "Divition" : divide });
 })
 
-app.get('/students/:name/:cmark/:mmark/:pmark', (req, res) => {
-    const name = req.params.name;
-    const cmark = req.params.cmark;
-    const mmark = req.params.mmark;
-    const pmark = req.params.pmark;
-    const total = parseInt(cmark ) + parseInt(mmark) + parseInt(pmark)
-    // console.log(data);
 
-    res.send('Name: ' +name +` Chemistry Mark : `+cmark +` Maths Mark : ` +mmark +` Physics Mark : `+pmark  +" Total = " + total)
-    const data = localStorage.setItem('date', {"name":name, "cmark":cmark, "mmark": mmark , "pmark" : pmark , "total": total})
-    console.log(data);
+app.use(router);
 
-})
-app.post('/', (req, res) => {
-  res.send('POST request to the homepage')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
